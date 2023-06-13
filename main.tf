7f3b78bf7393952a3cf7954b86d303c9fe3a831f
mukes137 <t4.cs.soft9@gmail.com>
2023-06-13 14:12:33 +0545

    changes

--- main.tf
+++ main.tf
@@ -14,44 +14,44 @@ provider "aws" {
    
 }
 
-# module "vpc" {
-#     source      = "./modules/vpc"
+module "vpc" {
+    source      = "./modules/vpc"
 
 
-# }
+}
 
-# module "ec2" {
-#     source                    = "./modules/ec2"
-#     vpc_id                    = module.vpc.my_vpc_id
-#     subnet_id                 = module.vpc.public_subnet
-#     security_group_id         = module.vpc.my_sg
+module "ec2" {
+    source                    = "./modules/ec2"
+    vpc_id                    = module.vpc.my_vpc_id
+    subnet_id                 = module.vpc.public_subnet
+    security_group_id         = module.vpc.my_sg
 
-# }
+}
 
-# module "db" {
-#     source                    = "./modules/rds"
-#     subnet_id                = module.vpc.private_subnet
-#     security_group_id        = module.vpc.my_sg
-#     vpc_id                    = module.vpc.my_vpc_id
-# }
+module "db" {
+    source                    = "./modules/rds"
+    subnet_id                = module.vpc.private_subnet
+    security_group_id        = module.vpc.my_sg
+    vpc_id                    = module.vpc.my_vpc_id
+}
 
 
 
-# module "lb" {
-#   source                      = "./modules/load_balancer"
-#   vpc_id                      = module.vpc.my_vpc_id
-#   subnet_id                   = module.vpc.public_subnet
-#   security_group_id           = module.vpc.my_sg
-#   instance_id                 = module.ec2.my_server_id
+module "lb" {
+  source                      = "./modules/load_balancer"
+  vpc_id                      = module.vpc.my_vpc_id
+  subnet_id                   = module.vpc.public_subnet
+  security_group_id           = module.vpc.my_sg
+  instance_id                 = module.ec2.my_server_id
   
-# }
+}
 
-# module "as" {
-#   source                      = "./modules/auto_scaling"
-#   target_group_arn            = module.lb.target_group_arn
-#   server_id                   = module.ec2.my_server_id
-#   subnet_id                   = module.vpc.public_subnet
-# }
+module "as" {
+  source                      = "./modules/auto_scaling"
+  target_group_arn            = module.lb.target_group_arn
+  server_id                   = module.ec2.my_server_id
+  subnet_id                   = module.vpc.public_subnet
+}
 
 module "sss" {
   source                        = "./modules/s3"
